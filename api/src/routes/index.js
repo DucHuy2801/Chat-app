9e540b68c3e254faa1b273032e5a3d6815ee751b
DucHuy2801 <theflash28012002@gmail.com>
2024-02-21 01:26:08 +0700

    add user route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,5 +4,6 @@ const express = require("express")
 const router = express.Router()
 
 route.use('/v1/api/auth', require('./auth'))
+route.use('/v1/api/user', require('./user'))
 
 module.exports = router
