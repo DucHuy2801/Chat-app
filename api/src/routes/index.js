2168ca1c76eb7ef07496d2fbbcec67ee42d7798e
DucHuy2801 <theflash28012002@gmail.com>
2024-02-21 13:51:12 +0700

    add post route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 route.use('/v1/api/auth', require('./auth'))
 route.use('/v1/api/user', require('./user'))
+route.use('/v1/api/post', require('./post'))
 
 module.exports = router
