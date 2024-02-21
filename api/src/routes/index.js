e3921a9f7a0881b82bc62eb34ba00f7e1256856e
DucHuy2801 <theflash28012002@gmail.com>
2024-02-21 14:41:31 +0700

    done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,5 +6,7 @@ const router = express.Router()
 route.use('/v1/api/auth', require('./auth'))
 route.use('/v1/api/user', require('./user'))
 route.use('/v1/api/post', require('./post'))
+route.use('/v1/api/conversation', require('./conversation'))
+route.use('/v1/api/message', require('./message'))
 
 module.exports = router
